d8cb7d84262cd9f7818cbfe5cbdc59ff163f27c5
Moshe <mosheygrunwald@gmail.com>
2018-10-03 14:55:31 -0400

    added requirements, setup.py and License

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='hebcal',
+    version='0.0.2.a0.dev0',
+    author='Moshe G',
+    author_email='themygcompany@gmail.com',
+    description='A Python package to manipulate location aware, Hebrew dates, times, and holidays.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TheBestMoshe/hebcal',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'convertdate==2.1.3',
+        'ephem==3.7.6.0',
+        'hebcal==0.0.1',
+        'py-dateutil==2.2',
+        'pyluach==0.3.0.dev1',
+        'pytz==2018.5',
+        'Shapely==1.6.4.post2',
+        'six==1.11.0',
+        'tzwhere==3.0.3',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha'
+    ],
+)
